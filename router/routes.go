34c4a7d938828d9fa3c1626f28e870375c234940
FelipeAugst <felipeaug92@gmail.com>
2024-03-12 00:25:50 -0300

    validations

--- router/routes.go
+++ router/routes.go
@@ -8,6 +8,7 @@ func ConfigRoutes(app *fiber.App) {
 	var routes []Route
 	routes = append(routes, routePecas...)
 	routes = append(routes, routeClientes...)
+	routes = append(routes, routeFornecedores......)
 
 	for _, route := range routes {
 		app.Add(route.method, route.path, route.handler)
