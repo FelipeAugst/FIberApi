19571d3b104424e0315445263ac03b43c97ed1cd
FelipeAugst <felipeaug92@gmail.com>
2024-03-22 00:08:31 -0300

    routes vendas

---
+++ router/vendas.go
@@ -0,0 +1,62 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+var routeVendas = []Route{
+	{
+		path:   "/vendas",
+		method: http.MethodGet,
+		handler: func(*fiber.Ctx) error {
+			return nil
+		},
+	},
+	{
+		path:   "/vendas",
+		method: http.MethodPost,
+		handler: func(*fiber.Ctx) error {
+			return nil
+		},
+	},
+
+	{
+		path:   "/vendas/:id",
+		method: http.MethodPost,
+		handler: func(*fiber.Ctx) error {
+			return nil
+		},
+	},
+
+	{
+		path:   "/vendas/peca/:filter",
+		method: http.MethodGet,
+		handler: func(*fiber.Ctx) error {
+			return nil
+		},
+	},
+	{
+		path:   "/vendas/fornecedor/:filter",
+		method: http.MethodGet,
+		handler: func(*fiber.Ctx) error {
+			return nil
+		},
+	},
+	{
+		path:   "/vendas",
+		method: http.MethodPut,
+		handler: func(*fiber.Ctx) error {
+			return nil
+		},
+	},
+
+	{
+		path:   "/vendas",
+		method: http.MethodDelete,
+		handler: func(*fiber.Ctx) error {
+			return nil
+		},
+	},
+}
