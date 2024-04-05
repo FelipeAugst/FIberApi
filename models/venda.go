e1f778b8324ba389751e3ad5da9782ec903470df
FelipeAugst <felipeaug92@gmail.com>
2024-04-05 02:23:26 -0300

    venda

--- models/venda.go
+++ models/venda.go
@@ -6,8 +6,8 @@ type Venda struct {
 	gorm.Model
 	Cliente
 	Peca
-	PecaID     uint
-	ClienteID  uint
+	PecaID     uint `gorm:"foreignKey:Peca.ID"`
+	ClienteID  uint `gorm:"foreignKey:Cliente.ID"`
 	Quantidade uint `json:"quantidade" gorm:"column:quantidade"`
 }
 
