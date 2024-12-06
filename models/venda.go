e02a0648f1e5cc888f37ea732cbc3669c8b66884
FelipeAugst <felipeaug92@gmail.com>
2024-12-06 00:22:37 -0300

    pedido de venda model

--- models/venda.go
+++ models/venda.go
@@ -8,6 +8,8 @@ type Venda struct {
 	gorm.Model
 	Peca            Peca    `json:"pecas"`
 	Cliente         Cliente `json:"cliente"`
+	PecaID          uint
+	ClienteID       uint
 	PedidoDeVendaID uint
 }
 
